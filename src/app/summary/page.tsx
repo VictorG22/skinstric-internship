a189f940ddaab68f80e72bc4d322f094086f7c2f
VictorG22 <victorgaona702@gmail.com>
2025-12-27 00:44:00 -0800

    resolved code rabbit conversations

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function SummaryPage() {
   return (
-    <div>
-      This is the summary page
+    <div className='relative w-full h-screen'>
+                <Link
+            href={"/select"}
+            className="absolute left-5 bottom-10 group flex gap-4 items-center z-20 cursor-pointer"
+          >
+            <Image
+              width={50}
+              height={50}
+              alt="left button"
+              src={"/left-btn.png"}
+              className="group-hover:scale-109 transition duration-200 "
+            />
+            <p className="uppercase font-semibold">back</p>
+          </Link>
     </div>
   )
 }
